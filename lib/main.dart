d327e7dd3af133d17bced017bd2159af019b2b20
jyjonep <williamj2013@163.com>
2019-07-11 13:44:27 +0800

    commit SystemUiOverlayStyle

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+
 import 'package:flutter_shoot/app.dart';
+import 'package:flutter_shoot/util/tools.dart';
 
 void main() {
   runApp(MyApp());
 
-
+  if (Platform.isAndroid) {
+    SystemUiOverlayStyle systemUiOverlayStyle =
+        SystemUiOverlayStyle(statusBarColor: Colors.transparent);
+    SystemChrome.setSystemUIOverlayStyle(systemUiOverlayStyle);
+  }
 }
-
-
-
--- lib/util/tools.dart
+++ lib/util/tools.dart
@@ -2,4 +2,6 @@ library tools;
 
 export 'dart:ui';
 export 'dart:async';
+export 'dart:io';
 export 'package:flutter/services.dart';
+
