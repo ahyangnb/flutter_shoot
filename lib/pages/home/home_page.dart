4229ce90d686999f55a1381b587fede51e11fdfc
jyjonep <williamj2013@163.com>
2019-07-16 11:30:04 +0800

    commit sqflite

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -16,3 +16,12 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
+
+
+class Dog {
+  final int id;
+  final String name;
+  final int age;
+
+  Dog({this.id, this.name, this.age});
+}
