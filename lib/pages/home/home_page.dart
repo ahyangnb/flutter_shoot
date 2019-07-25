3c2c1bad081528b6835add7e9d4461658e6550cf
ahyang <zonggeu@163.com>
2019-07-25 23:14:02 +0800

    commit remove camera

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart';
-import 'package:camera/camera.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -9,14 +8,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  Future<void> run() async {
-    final cameras = await availableCameras();
-
-    final firstCamera = cameras.first;
-
-    return firstCamera;
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,35 +16,5 @@ class _HomePageState extends State<HomePage> {
         centerTitle: true,
       ),
     );
-  }
-}
-
-class TakePictureScreen extends StatefulWidget {
-  final CameraDescription camera;
-
-  TakePictureScreen(this.camera);
-
-  @override
-  _TakePictureScreenState createState() => _TakePictureScreenState();
-}
-
-class _TakePictureScreenState extends State<TakePictureScreen> {
-  CameraController _cameraC;
-
-  Future<void> _ini;
-
-  @override
-  void initState() {
-    super.initState();
-    _cameraC = CameraController(
-      widget.camera,
-      ResolutionPreset.low,
-      enableAudio: true,
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Container();
   }
 }
